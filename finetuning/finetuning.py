11e4ff596ce122e7f9a009b23ab59f923ad9eb49
Leonard Püttmann <leopuettmann@gmail.com>
2024-10-30 23:13:31 +0100

    Finished rough training script

--- finetuning/finetuning.py
+++ finetuning/finetuning.py
@@ -18,10 +18,13 @@ from datasets import load_dataset
 from trl import SFTTrainer, setup_chat_format
 
 from huggingface_hub import login
+from datasets import *
 
 import os
+import glob 
+import json
 
-hf_token = os.get_env("HUGGINGFACE_API_KEY")
+hf_token = os.getenv("HUGGINGFACE_API_KEY")
 login(token=hf_token)
 
 # wandb.login(key=wb_token)
@@ -31,13 +34,13 @@ login(token=hf_token)
 #     anonymous="allow"
 # )
 
-# Set torch dtype and attention implementation
-if torch.cuda.get_device_capability()[0] >= 8:
-    torch_dtype = torch.bfloat16
-    attn_implementation = "flash_attention_2"
-else:
-    torch_dtype = torch.float16
-    attn_implementation = "eager"
+# # Set torch dtype and attention implementation
+# if torch.cuda.get_device_capability()[0] >= 8:
+#     torch_dtype = torch.bfloat16
+#     attn_implementation = "flash_attention_2"
+# else:
+torch_dtype = torch.float16
+attn_implementation = "eager"
 
 # QLoRA config
 bnb_config = BitsAndBytesConfig(
@@ -92,6 +95,58 @@ model = get_peft_model(model, peft_config)
 
 new_model = "llama-3.2-1b-it-finetuned"
 
+# prep the dataset
+def load_json_data(folder_path, key):
+    data = []
+    for file_path in glob.glob(folder_path + '/*.json'):
+        with open(file_path, 'r') as file:
+            json_data = json.load(file)
+            data.extend(json_data[key])
+    return data
+
+raw_pages = load_json_data("../data_prep/data", key="ocr_results")
+cleaned_pages = load_json_data("../data_prep/data", key="cleaned_pages")
+
+test_size = 0.2
+train_size = int(len(cleaned_pages) * (1 - test_size))
+train_raw_pages, test_raw_pages = raw_pages[:train_size], raw_pages[train_size:]
+train_cleaned_pages, test_cleaned_pages = cleaned_pages[:train_size], cleaned_pages[train_size:]
+
+dataset = DatasetDict({
+    'train': Dataset.from_dict({"raw_pages": train_raw_pages[:len(train_cleaned_pages)], "cleaned_pages": train_cleaned_pages}),
+    'test': Dataset.from_dict({"raw_pages": test_raw_pages[:len(test_cleaned_pages)], "cleaned_pages": test_cleaned_pages})
+})
+
+base_model = "unsloth/Llama-3.2-1B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
+
+instruction = """
+Du bist ein Experte für Textbereinigung. Deine Aufgabe ist es, einen Eingabetext zu bereinigen, der aus einem PDF-Dokument extrahiert wurde. Der Inhalt ist immer nur von einer einzelnen Seite, es sollte also nicht zu viel Text auf einmal sein. Es ist sehr wichtig, dass keine Daten und Informationen verloren gehen und dass die Originaltexte in keiner Weise verändert werden!
+Antworte ausschließlich in Deutsch und keiner anderen Sprache.
+
+Du hast die folgenden Aufgaben:
+- Entferne alle seltsamen Textteile und Sonderzeichen.
+- Entferne alle unnötigen Leerzeichen und Zeilenumbrüche.
+- Organisiere die Formatierung.
+- Korrektur von Rechtschreibfehlern.
+- Handling von Formatierungsfehlern.
+
+Gib nur den bereinigten und formatierten Text zurück und nichts anderes! Füge keinen eigenen Text hinzu! Achte auf Vollständigkeit, es darf kein Inhalt verloren gehen und es muss alles 100 % vollständig sein!
+"""
+
+def format_chat_template(row):
+    
+    row_json = [{"role": "system", "content": instruction},
+               {"role": "user", "content": row["raw_pages"]},
+               {"role": "assistant", "content": row["cleaned_pages"]}]
+    
+    row["text"] = tokenizer.apply_chat_template(row_json, tokenize=False)
+    return row
+
+dataset = dataset.map(
+    format_chat_template
+)
+
 #Hyperparamter
 training_arguments = TrainingArguments(
     output_dir=new_model,
@@ -109,7 +164,8 @@ training_arguments = TrainingArguments(
     fp16=False,
     bf16=False,
     group_by_length=True,
-    report_to="wandb"
+    report_to=None,
+    remove_unused_columns=False
 )
 
 # Setting sft parameters
@@ -126,8 +182,8 @@ trainer = SFTTrainer(
 )
 
 trainer.train()
-wandb.finish()
+#wandb.finish()
 
 # Save the fine-tuned model
 trainer.model.save_pretrained(new_model)
-trainer.model.push_to_hub(new_model, use_temp_dir=False)
+#trainer.model.push_to_hub(new_model, use_temp_dir=False)
